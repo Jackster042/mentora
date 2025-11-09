0f5af7c33035e0b0884c8901f2878fc6fe40ec13
Nemanja Stojanovic <feemail042@gmail.com>
2025-11-10 00:27:26 +0100

    Bookmark implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,47 @@
-import React from 'react'
+import React from "react";
 
 import CompanionCard from "@/components/companion/CompanionCard";
 import CompanionList from "@/components/companion/CompanionList";
-import {getSubjectColor} from "@/lib/utils";
-import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
 import Cta from "@/components/companion/CTA";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+  // TODO: ADD LANDING PAGE
+  //   const { userId } = await auth();
+  //   if (!userId) redirect("/sign-in");
 
-    const companions = await getAllCompanions({ limit: 3 });
-    const recentSessionCompanions = await getRecentSessions(10)
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionCompanions = await getRecentSessions(10);
 
   return (
     <main>
-        <h1>Popular Companions</h1>
-        <section className="home-section">
-            {companions.map((companion) => (
-                <CompanionCard
-                    key={companion.id}
-                    {...companion}
-                    color={getSubjectColor(companion.subject)}
-                />
-            ))}
-        </section>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
 
-        <section className={"home-section"}>
-            <CompanionList
-            title="Recently completed sessions"
-            companions={recentSessionCompanions}
-            classNames="w-2/3 max-lg:w-full"
-            />
-            <Cta />
-        </section>
+      <section className={"home-section"}>
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessionCompanions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
