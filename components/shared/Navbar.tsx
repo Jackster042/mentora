5924b5819c8f79b048d4bef8d1346461d15cf66f
Nemanja Stojanovic <feemail042@gmail.com>
2025-11-10 19:46:54 +0100

    logo change , gitignore update

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -17,16 +17,17 @@ export const Navbar =  () => {
     return (
         <Fragment>
         <nav className="navbar">
-            <Link href="/">
-                <div className="flex items-center gap-2.5 cursor-pointer">
-                    <Image
-                    src="/images/logo.svg"
-                    alt="Logo"
-                    width={46}
-                    height={44}
-                    />
-                </div>
-            </Link>
+              <Link href="/" className="group">
+      <div className="flex items-center gap-2.5 cursor-pointer relative">
+        <Image src="/images/logo2.png" alt="Logo" width={30} height={20} />
+        <p className="text-3xl font-semibold ml-4 hidden md:block relative">
+          Mentora
+          <span
+            className="absolute left-0 bottom-0 w-0 h-[2px] bg-gray-500 transition-all duration-300 ease-out group-hover:w-full"
+          ></span>
+        </p>
+      </div>
+    </Link>
  
     <div className={"flex items-center gap-8"}>
                 <SignedIn>
